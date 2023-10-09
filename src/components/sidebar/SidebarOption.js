f5c8504437c6a565a10402e10406e1f454102865
Keigo Takahashi <keigo@TK.local>
2023-10-10 06:18:36 +0900

    MUI 導入

---
+++ src/components/sidebar/SidebarOption.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SidebarOption({ text, Icon }) {
+  return (
+    <div>
+        <Icon />
+        <h2>{ text }</h2>
+    </div>
+
+  )
+}
+
+export default SidebarOption
